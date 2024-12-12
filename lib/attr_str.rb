3d6ed9cb599713c8a069ea2e496d0e9865d09db1
Caio Chassot <dev@caiochassot.com>
2024-12-12 11:40:47 -0300

    attr_str ruby wrapper

---
+++ lib/attr_str.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+require "ffi"
+
+module NSAttributedString
+  extend FFI::Library
+  ffi_lib "#{__dir__}/attr_str.dylib"
+
+  attach_function :attributed_string_unarchive, [:pointer, :size_t], :pointer
+  attach_function :attributed_string_describe,  [:pointer, :size_t], :pointer
+  attach_function :free, [:pointer], :void
+
+  def self.unarchive(bin) = handle_result(bin) { |ptr, len| attributed_string_unarchive(ptr, len) }
+  def self.describe(bin)  = handle_result(bin) { |ptr, len| attributed_string_describe(ptr, len) }
+
+  private
+  def self.handle_result(bin)
+    return nil if bin.nil?
+
+    ptr = FFI::MemoryPointer.new :char, bin.bytesize
+    ptr.put_bytes 0, bin
+
+    result = yield ptr, bin.bytesize
+    return nil if result.null?
+
+    str = result.read_string
+    free result
+    str
+  end
+end
+
+if __FILE__ == $0
+  bin = STDIN.read
+  puts "Decoded: #{NSAttributedString.unarchive(bin) || 'null'}"
+  puts "Description: #{NSAttributedString.describe(bin) || 'null'}"
+end
