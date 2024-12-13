744214f86a9c2c70ea9fbf5d9f8c57adb61923e2
Caio Chassot <dev@caiochassot.com>
2024-12-13 12:18:00 -0300

    Revert "keyed_archive stop raising on bad data, just return as such"
    This reverts commit d7be6bf210d153127835eee1a4abdd4ac5ce91d1.

--- lib/keyed_archive.rb
+++ lib/keyed_archive.rb
@@ -11,16 +11,13 @@ class NSKeyedArchive
 
   def initialize(data, strip_meta: false)
     data = xml_from_binary_plist data if data.start_with?("bplist")
-    return @data = nil unless data.start_with?("<?xml", "<plist")
-
+    raise "no xml" unless data.start_with?("<?xml", "<plist")
     @data       = Plist.parse_xml(data)
     @objects    = @data["$objects"]
     @strip_meta = strip_meta
   end
 
   def unarchive
-    return { "_ERROR": "BAD DATA" } if data.nil?
-
     top = @data["$top"]
     return parse_object(top["root"]) if top["root"]
 
