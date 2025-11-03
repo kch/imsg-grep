913fb03435a74a53f5447a1054faa7cb712b471f
Caio Chassot <dev@caiochassot.com>
2025-11-03 20:22:12 -0300

    cleanup

--- lib/messages.rb
+++ lib/messages.rb
@@ -26,9 +26,7 @@ REGEX_CACHE = Hash.new { |h,k| h[k] = Regexp.new(k, Regexp::IGNORECASE) }
 $db.create_function("regexp", 2)               { |f, rx, text| f.result = REGEX_CACHE[rx].match?(text) ? 1 : 0 }
 $db.create_function("plusdigits", 1)           { |f, text| f.result = text.delete("^0-9+") }
 $db.create_function("unarchive_keyed", 1)      { |f, text| f.result = NSKeyedArchive.unarchive(text).to_json }
-$db.create_function("unarchive_keyed_strip", 1){ |f, text| f.result = NSKeyedArchive.unarchive(text, strip_meta: true).to_json }
 $db.create_function("unarchive_attributed", 1) { |f, text| f.result = NSAttributedString.unarchive text }
-# $db.create_function("describe_attributed", 1)  { |f, text| f.result = NSAttributedString.describe text }
 
 # all contacts, like:
 # id      | 42
@@ -141,7 +139,6 @@ MESSAGES_DECODED_QUERY = <<~SQL
     m.ROWID as id,
     m.guid,
     IIF(m.is_from_me, m.destination_caller_id, h.id) as sender_handle,
-    datetime((m.date / 1000000000) + 978307200, 'unixepoch') as utc_time,
     IIF(m.destination_caller_id IS NOT NULL, json_insert(p.participant_handles, '$[#]', m.destination_caller_id), p.participant_handles) as participant_handles,
     IIF(m.attributedBody IS NOT NULL, unarchive_attributed(m.attributedBody), NULL) as text_decoded,
     IIF(m.payload_data IS NOT NULL, unarchive_keyed(payload_data), NULL) as payload,
