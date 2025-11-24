10776ddb57e7f83bcb9b53a6461273de191aa2b8
Caio Chassot <dev@caiochassot.com>
2025-11-24 02:29:17 -0300

    print_query: simplify

--- lib/print_query.rb
+++ lib/print_query.rb
@@ -79,15 +79,10 @@ end
 
 def print_query(db, sql)
   puts "\n#{'=' * 80}"
-  cols = []
 
-  db.execute2(sql) do |row|
-    if cols.empty?
-      cols = row
-      next
-    end
-    width = cols.map(&:length).max
-
+  cols, *rows = db.execute2(sql)
+  width = cols.map(&:length).max
+  rows.each do |row|
     row.each_with_index do |val, i|
       val = val.nil? ? "NULL" : val.to_s
       puts "#{cols[i].ljust(width, ' ')} : #{val.gsub("\n", "\n   " + ' ' * width)}"
