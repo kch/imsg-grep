116406183b9d7fae1117aba782efdb0d4b42aeb9
Caio Chassot <dev@caiochassot.com>
2025-11-29 15:20:36 -0300

    rename warn to warn!, move --all check earlier, fix -L overrides msging

--- lib/grep/strop_utils.rb
+++ lib/grep/strop_utils.rb
@@ -34,7 +34,7 @@ class Strop::Result
   def compact_singles!(*labels)
     labels.flatten.filter_map{ (os = opts[[it]]).size > 1 and [it, os] }.each do |label, opts|
       names = opts.map(&:_name).uniq.join(", ")
-      warn "multiple #{names} options specified. Last takes precedence."
+      warn! "multiple #{names} options specified. Last takes precedence."
       replace self - opts[...-1]
     end
   end
