3b9dca3a7142fea273d8403ed11ad410ad72cb38
Caio Chassot <dev@caiochassot.com>
2025-12-05 22:03:57 -0300

    move test

--- test/date_test.rb
+++ test/date_test.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 require "minitest/autorun"
-require_relative "../../lib/grep/date"
+require_relative "../lib/grep/date"
 
 class DateArgTest < Minitest::Test
   include DateArg
